a38e7c257942d375062680acd2dc7a6fe2b03db6
akshaykriish25 <91551580+akshaykriish25@users.noreply.github.com>
2021-09-28 20:24:42 +0530

    Add files via upload

---
+++ Arthm.java
@@ -0,0 +1,20 @@
+package testFile;
+
+public class Arthm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;  
+		int b=5;  
+		System.out.println(a+b); 
+		System.out.println(a-b);  
+		System.out.println(a*b); 
+		System.out.println(a/b); 
+		System.out.println(a%b);  
+		
+		int min=(a<b)?a:b;  
+		System.out.println(min);  
+
+	}
+
+}
---
+++ Helo.java
@@ -0,0 +1,11 @@
+package pro1;
+
+public class Helo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Hello");
+
+	}
+
+}
