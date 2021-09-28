502a0e2de32415b6915a04800258abd5bbf5d290
akshaykriish25 <akshaykriish2@gmail.com>
2021-09-28 20:39:53 +0530

    commit msg 1;

--- Helo.java
+++ Helo.java
@@ -5,7 +5,8 @@ public class Helo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.print("Hello");
-
+System.out.print("world");
+System.out.print("world");
 	}
 
 }
